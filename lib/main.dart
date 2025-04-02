ddda25ef39432886e6bf046d460fb1dbf91064c1
HEYSHEED <mahshidebshirazi@gamil.com>
2025-04-01 23:01:19 -0400

    USING HIVE - EDIT TASK

--- lib/main.dart
+++ lib/main.dart
@@ -324,9 +324,11 @@ class _TaskItemState extends State<TaskItem> {
     }
     return InkWell(
       onTap: () {
-        setState(() {
-          widget.task.isCompleted = !widget.task.isCompleted;
-        });
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => EditTaskScreen(task: widget.task),
+          ),
+        );
       },
       child: Container(
         height: TaskItem.height,
@@ -344,7 +346,14 @@ class _TaskItemState extends State<TaskItem> {
         ),
         child: Row(
           children: [
-            CheckBoxShape(value: widget.task.isCompleted, color: Colors.green),
+            MyCheckBox(
+              value: widget.task.isCompleted,
+              onTap: () {
+                setState(() {
+                  widget.task.isCompleted = !widget.task.isCompleted;
+                });
+              },
+            ),
             const SizedBox(width: 16),
             Expanded(
               child: Text(
@@ -352,7 +361,7 @@ class _TaskItemState extends State<TaskItem> {
                 maxLines: 1,
                 overflow: TextOverflow.ellipsis,
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: 15,
                   decoration:
                       widget.task.isCompleted
                           ? TextDecoration.lineThrough
@@ -374,6 +383,38 @@ class _TaskItemState extends State<TaskItem> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MyCheckBox extends StatelessWidget {
+  final bool value;
+  final Function() onTap;
+  const MyCheckBox({super.key, required this.value, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 24,
+        height: 24,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border:
+              !value ? Border.all(color: secondaryTextColor, width: 2) : null,
+          color: value ? primaryColor : null,
+        ),
+        child:
+            value
+                ? Icon(
+                  CupertinoIcons.check_mark,
+                  size: 16,
+                  color: themeData.colorScheme.onPrimary,
+                )
+                : null,
       ),
     );
   }
